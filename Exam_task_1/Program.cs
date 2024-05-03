47378afd2050ecc81796f57ebaf6ae91d224d7ce
Kapebah <sh1n0sh1@mail.ru>
2024-05-03 13:32:27 +0300

    task_1 complete

---
+++ Exam_task_1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Exam_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            mark1:
+            try
+            {
+                ForegroundColor = ConsoleColor.DarkCyan;
+                WriteLine("Задание №1\n");
+                ResetColor();
+
+                List<Rectangle> rectangles = new List<Rectangle>();
+                bool flag = true;
+                int count_1 = 1;
+                int count_2 = 1;
+
+                while (flag)
+                {
+                    WriteLine("1 - Создать прямоугольник" +
+                    "\n2 - Высчитать периметр" +
+                    "\n3 - Высчитать площадь");
+                    switch (ReadKey(true).Key)
+                    {
+                        case ConsoleKey.D1:
+                            Clear();
+                            rectangles.Add(Rectangle.Input());
+                            WriteLine();
+                            break;
+                        case ConsoleKey.D2:
+                            Clear();
+                            foreach (var rect in rectangles)
+                            {
+                                WriteLine($"Прямоугольник №{count_1++}");
+                                WriteLine(rect.Perimeter);
+                                WriteLine();
+                            }
+                            count_1 = 1;
+                            break;
+                        case ConsoleKey.D3:
+                            Clear();
+                            foreach (var rect in rectangles)
+                            {
+                                WriteLine($"Прямоугольник №{count_2++}");
+                                WriteLine(rect.Area);
+                                WriteLine();
+                            }
+                            count_2 = 1;
+                            break;
+
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+
+                WriteLine("\nНажмите Enter для продолжения. . .");
+                if (ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    goto mark1;
+                }
+            }
+            ReadKey();
+        }
+    }
+}
---
+++ Exam_task_1/Rectangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Exam_task_1
+{
+    class Rectangle
+    {
+        /// <summary>
+        /// Стороны прямоугольника
+        /// </summary>
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        /// <summary>
+        /// Конструкторы
+        /// </summary>
+        public Rectangle() { }
+        public Rectangle(double side1, double side2)
+        {
+            Side1 = side1; 
+            Side2 = side2;
+            Area = AreaCalculator();
+            Perimeter = PerimeterCalculator();
+        }
+        /// <summary>
+        /// Площадь
+        /// </summary>
+        /// <returns></returns>
+        public double AreaCalculator()
+        {
+            return Side1 * Side2;
+        }
+        /// <summary>
+        /// Периметр
+        /// </summary>
+        /// <returns></returns>
+        public double PerimeterCalculator()
+        {
+            return (Side1 + Side2) * 2;
+        }
+        /// <summary>
+        /// Свойства площади и периметра
+        /// </summary>
+        public double Area { get; }
+        public double Perimeter { get; }
+        /// <summary>
+        /// Ввод с клавиатуры
+        /// </summary>
+        /// <returns></returns>
+        public static Rectangle Input()
+        {
+            Write("Введите первую сторону: ");
+            double side1 = Convert.ToDouble(ReadLine());
+            Write("Введите вторую сторону: ");
+            double side2 = Convert.ToDouble(ReadLine());
+
+            return new Rectangle(side1, side2);
+        }
+    }
+}
